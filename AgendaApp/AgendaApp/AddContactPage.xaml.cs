5769f032951ca6fa780ec21bc7ed3b3ca7d53186
gustavomirandag <gustavomirandag@gmail.com>
2018-09-15 16:16:22 -0300

    CrossPlatform AgendaSolution with manual refresh contacts.

---
+++ AgendaApp/AgendaApp/AddContactPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using AgendaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AgendaApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AddContactPage : ContentPage
+	{
+        private List<Contact> _contacts;
+
+        public event EventHandler ContactAdded;
+		public AddContactPage (List<Contact> contacts)
+		{
+			InitializeComponent ();
+            _contacts = contacts;
+		}
+
+        private void ButtonCreate_Clicked(object sender, EventArgs e)
+        {
+            //Add the new contact to database
+            _contacts.Add(CreateNewContact());
+
+            ContactAdded(this, new EventArgs());
+
+            //Pop Modal Async (go back to MainPage)
+            Navigation.PopModalAsync(true);
+        }
+
+        /// <summary>
+        /// Function that creates a new contact
+        /// </summary>
+        /// <returns></returns>
+        private Contact CreateNewContact()
+        {
+            var contact = new Contact();
+            contact.Name = EntryName.Text;
+            contact.Phone = EntryPhone.Text;
+            contact.Email = EntryEmail.Text;
+
+            return contact;
+        }
+
+        private void ButtonCancel_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync(true);
+        }
+    }
+}
--- AgendaApp/AgendaApp/MainPage.xaml.cs
+++ AgendaApp/AgendaApp/MainPage.xaml.cs
@@ -1,17 +1,67 @@
-﻿using System;
+﻿using AgendaApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace AgendaApp
 {
     public partial class MainPage : ContentPage
     {
+        private static List<Contact> _contacts;
+
         public MainPage()
         {
             InitializeComponent();
+            _contacts = new List<Contact>();
+            FillContacts(); //FillContacts for the first time
+        }
+
+        public void FillContacts()
+        {
+            StackLayoutContactsList.Children.Clear();
+            foreach(var contact in _contacts)
+            {
+                var stackLayoutContact = new StackLayout();
+                var labelName = new Label
+                {
+                    Text = contact.Name
+                };
+                var labelPhone = new Label
+                {
+                    Text = contact.Phone
+                };
+                var labelEmail = new Label
+                {
+                    Text = contact.Email
+                };
+                stackLayoutContact.Children.Add(labelName);
+                stackLayoutContact.Children.Add(labelPhone);
+                stackLayoutContact.Children.Add(labelEmail);
+                StackLayoutContactsList.Children.Add(stackLayoutContact);
+            }
+        }
+
+        private void ButtonAddContact_Clicked(object sender, EventArgs e)
+        {
+            AddContact();
+        }
+
+        private void AddContact()
+        {
+            var addContactPage = new AddContactPage(_contacts);
+
+            addContactPage.ContactAdded += AddContactPage_ContactAdded;
+
+            Navigation.PushModalAsync(addContactPage, true);
+        }
+
+        private void AddContactPage_ContactAdded(object sender, EventArgs e)
+        {
+            FillContacts();
         }
     }
 }
